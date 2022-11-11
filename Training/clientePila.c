2c1448daa722a0db8f21e618c7074c8ab07b386e
Kary <karyb3008@gmail.com>
2022-11-11 11:11:45 -0600

    Pila estática

--- Training/clientePila.c
+++ Training/clientePila.c
@@ -5,13 +5,38 @@ int main()
 {
 	Pila *p; //regresa un apuntador
 	p = crear();
-	if(is_empty(*p)==1) //mandamos el contenido de la pila
-		printf("La pila esta vacia"); //como apenas se creo está vacía
+	if(is_empty(*p)) //mandamos el contenido de la pila
+		printf("La pila esta vacia\n"); //como apenas se creo está vacía
 	
-	if(is_full(*p)==1)
-		printf("La pila esta llena");
+	if(is_full(*p))
+		printf("La pila esta llena\n");
 	else
-		printf("La pila no esta llena");
+		printf("La pila no esta llena\n");
+		
+	if(push(p,4))
+		printf("Elemento ingresado\n");	
+	else
+		printf("La pila esta llena\n");
+	
+	if(push(p,5))
+		printf("Elemento ingresado\n");	
+	else
+		printf("La pila esta llena\n");
+		
+	if(push(p,1))
+		printf("Elemento ingresado\n");	
+	else
+		printf("La pila esta llena\n");
+		
+	if(!is_empty(*p)) //sino está vacía
+	{
+		//int dato=pop(p); 
+		//printf("El dato que sale: %d", dato); //indica el ultimo dato tiene que ser el uno
+		printf("El dato que sale: %d\n", pop(p));
+		printf("El dato que sale: %d\n", pop(p));
+		printf("El dato que sale: %d\n", pop(p));
+	}
+		
 	return 0;
 	
 }
--- Training/pila.h
+++ Training/pila.h
@@ -38,3 +38,31 @@ int is_full(Pila pila)
 	else 
 	return 0;
 }
+
+int push(Pila *pila, int dato) //recibimos un apuntador para que la pila original sea modificada
+{
+	if(pila->total<maxSize) //preguntar si no está llena, para insertar un elemento
+	{
+		pila->datos[pila->indice]=dato; //pila en su atributo datos, dónde indica el indice donde quiero guardar el dato que me acabam de enviar en ese paramentro(dato)
+		pila->indice++; //una vez insertado el valor, el índice se desplaza al siguiente.
+		pila->total++; //y cuando insertamos, el total se incrementa.
+		return 1; //en caso de que sí, avisa que si se pudo insertar
+		
+	}
+	else  //en caso contrario no inserta un elemento
+		return 0;
+}
+
+int pop(Pila *pila)
+{
+	int aux;
+	if(pila->total > 0) //si la pila es mayor a cero es porque tiene elementos que eliminar
+	{
+		aux = pila->datos[pila->indice-1]; //guarda el dato e indice en el auxiliar
+		pila->indice--; //se decrementa el índice
+		pila->total--; //se decrementa el total
+		return aux;
+	}
+	else
+		return -1;
+}
