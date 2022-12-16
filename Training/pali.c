413443f00fee4b52988728c076501137d514c26c
Kary <karyb3008@gmail.com>
2022-12-15 22:24:02 -0600

    Palíndromo.

--- Training/pali.c
+++ Training/pali.c
@@ -1,20 +1,60 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #define true 1
 #define false 0
 #define max 30
 
 typedef struct 
 {
-	short int tope; //último elemento que entra
-	short int vector[max];
+	int tope; //último elemento que entra
+	char vector[max];
 } stack;
+
+void CrearPila(stack *Pila);
+int isEmpty(stack *Pila);
+int isFull(stack *Pila);
+void add(stack *Pila, char n);
+void Delete(stack *Pila);
+int ultimo(stack *Pila);
+
+
+int main()
+{
+	char *palabra = "natan";
+	
+	int x = 0, l = strlen(palabra), y = 0;
+	char paludismo[l];
+	
+	stack Pila;
+	CrearPila(&Pila);
+	
+	for(x = 0; x < l; x++)
+	{
+		add(&Pila, palabra[x]);
+	}
+	
+	while(!isEmpty(&Pila))
+	{
+		paludismo[y] = Pila.vector[Pila.tope];
+		y++;
+		Delete(&Pila);
+	}
+	
+	if(strcmp(palabra, paludismo) == 0)
+		printf("Si es palindromo");
+	else
+		printf("No es palindromo");
+			
+	return true;
+}
 
 void CrearPila(stack *Pila)
 {
 	Pila->tope = -1;
 }
 
-int vacia(stack *Pila)
+int isEmpty(stack *Pila)
 {
 	if(Pila->tope == -1)
 		return true;
@@ -22,7 +62,7 @@ int vacia(stack *Pila)
 		return false;
 }
 
-int Llena(stack *Pila)
+int isFull(stack *Pila)
 {
 	if(Pila->tope == max - 1)
 		return true;
@@ -30,61 +70,31 @@ int Llena(stack *Pila)
 		return false;
 }
 
-void add(stack *Pila, int n)
+void add(stack *Pila, char n)
 {
-	if(!Llena(Pila))
+	if(!isFull(Pila))
 	{
 		Pila->tope++;
 		Pila->vector[Pila->tope] = n;
-		printf("Se ha metido  el elemento: %i\n", Pila->vector[Pila->tope]);
+		printf("Se ha metido  el elemento: %c\n", Pila->vector[Pila->tope]);
 	}
 	else
 		printf("La pila esta llena");
 }
 
 //elimina cuando no está vacía
-void Del(stack *Pila)
+void Delete(stack *Pila)
 {
-	if(!vacia(Pila))
+	if(!isEmpty(Pila))
 	{
 		int tmp = Pila->tope;
 		Pila->tope--;
-		printf("Se extrajo el elemento %i \n", Pila->vector[tmp]);
+		printf("Se extrajo el elemento %c \n", Pila->vector[tmp]);
 	}
 }
 
 int ultimo(stack *Pila)
 {
-	//if(!vacia(Pila))
 		return Pila->vector[Pila->tope];
-	//else 
-	//{
-	//	printf("Error, la pila esta vacia");
-		//return false;
-	//}
-}
-
-int main()
-{
-	stack Pila;
-	CrearPila(&Pila);
-	add(&Pila, 5);
-	add(&Pila, 10);
-	add(&Pila, 8);
-	add(&Pila, 12);
-	
-	printf("Ultimo: %i \n", ultimo(&Pila));
-	
-	Del(&Pila);
-	printf("Ultimo: %i \n", ultimo(&Pila));
-	Del(&Pila);
-	printf("Ultimo: %i \n", ultimo(&Pila));
-	Del(&Pila);
-	printf("Ultimo: %i \n", ultimo(&Pila));
-	Del(&Pila);
-	printf("Ultimo: %i \n", ultimo(&Pila));
-	
-		
-	return true;
 }
 
