d43ef3b81c1a5b8620e2df4a76df1503ba95be7f
Kary <karyb3008@gmail.com>
2022-12-15 22:37:13 -0600

    Número Capicua

---
+++ numCapicua.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define true 1
+#define false 0
+#define maxSize 10
+
+typedef struct 
+{
+	int tope; //último elemento que entra
+	int num[maxSize];
+} stack;
+
+void CrearPila(stack *Pila);
+int isEmpty(stack *Pila);
+int isFull(stack *Pila);
+void add(stack *Pila, int n);
+void Delete(stack *Pila);
+int ultimo(stack *Pila);
+
+
+int main()
+{
+	char *palabra = "1234321";
+	
+	int x = 0, l = strlen(palabra), y = 0;
+	char paludismo[l];
+	
+	stack Pila;
+	CrearPila(&Pila);
+	
+	for(x = 0; x < l; x++)
+	{
+		add(&Pila, palabra[x]);
+	}
+	
+	while(!isEmpty(&Pila))
+	{
+		paludismo[y] = Pila.num[Pila.tope];
+		y++;
+		Delete(&Pila);
+	}
+	
+	if(strcmp(palabra, paludismo) == 0)
+		printf("Si es palindromo");
+	else
+		printf("No es palindromo");
+			
+	return true;
+}
+
+void CrearPila(stack *Pila)
+{
+	Pila->tope = -1;
+}
+
+int isEmpty(stack *Pila)
+{
+	if(Pila->tope == -1)
+		return true;
+	else
+		return false;
+}
+
+int isFull(stack *Pila)
+{
+	if(Pila->tope == maxSize - 1)
+		return true;
+	else
+		return false;
+}
+
+void add(stack *Pila, int n)
+{
+	if(!isFull(Pila))
+	{
+		Pila->tope++;
+		Pila->num[Pila->tope] = n;
+		printf("Se ha metido  el elemento: %c\n", Pila->num[Pila->tope]);
+	}
+	else
+		printf("La pila esta llena");
+}
+
+//elimina cuando no está vacía
+void Delete(stack *Pila)
+{
+	if(!isEmpty(Pila))
+	{
+		int tmp = Pila->tope;
+		Pila->tope--;
+		printf("Se extrajo el elemento %c \n", Pila->num[tmp]);
+	}
+}
+
+int ultimo(stack *Pila)
+{
+		return Pila->num[Pila->tope];
+}
